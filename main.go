860c5d2e38811e0ff0f857da18995771f959cb54
Connor Zanin <cnnrznn@gmail.com>
2024-02-26 12:31:10 -0700

    Initial debug pass

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"sync"
+
+	"github.com/fsnotify/fsnotify"
+)
+
+func main() {
+	log.SetFlags(log.Lshortfile)
+	log.Println("Initialized")
+
+	rootDir := os.Getenv("DOLPHIN_EMU_USERPATH")
+	saveDir := fmt.Sprintf("%v/StateSaves", rootDir)
+	GCDir := fmt.Sprintf("%v/GC", rootDir)
+	log.Printf("Monitoring: \n%v\n%v\n", saveDir, GCDir)
+
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer watcher.Close()
+
+	//watcher.Add(saveDir)
+	watcher.Add(GCDir)
+
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+
+	go handleWatcher(watcher, wg)
+
+	wg.Wait()
+}
+
+func handleWatcher(watcher *fsnotify.Watcher, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	for {
+		select {
+		case event, ok := <-watcher.Events:
+			log.Println(event, ok)
+			if ok {
+				if err := handleEvent(event); err != nil {
+					log.Println(err)
+				}
+			}
+		case err, ok := <-watcher.Errors:
+			log.Println(err, ok)
+		}
+	}
+}
+
+func handleEvent(event fsnotify.Event) error {
+	if event.Op != fsnotify.Create {
+		return nil
+	}
+
+	if strings.HasSuffix(event.Name, ".tmp") {
+		return nil
+	}
+
+	return nil
+}
